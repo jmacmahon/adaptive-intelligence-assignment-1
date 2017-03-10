6375b8b3b47c7003e7ee7f8b1eb6159d1bc1d6b8
Joe MacMahon <joe@macmahon.eu>
2017-03-10 15:47:39 +0000

    Restructured Data object, including reducing

--- assignment/data.py
+++ assignment/data.py
@@ -5,26 +5,19 @@ class Data(object):
         self._raw_data = raw_data
         self._labels = labels
 
-    def get_train_letter(self, index):
-        return Letter(data=self._raw_data[index, :],
-                      label=self._labels[index],
-                      index=index)
+    def __getitem__(self, index):
+        return (self._labels[index], self._raw_data[index, :])
 
-    def __iter__(self):
-        for i in range(self._raw_data.shape[0]):
-            yield self.get_train_letter(i)
+    def _set_reducer(self, reducer):
+        self._reducer = reducer
+        reducer.train(self._raw_data)
 
+    reducer = property(fset=_set_reducer)
 
-class Letter(object):
-    def __init__(self, data, label=None, shape=(28, 28), index=None):
-        self.data = data
-        self.label = label
-        self._shape = shape
-        self._index = index
+    def reduce(self):
+        return Data(raw_data=self._reducer.reduce(self._raw_data),
+                    labels=self._labels)
 
-    def show(self):
-        show_image(self.data, self._shape)
-
-    def __repr__(self):
-        return 'Letter<index: {}, label: {}>'.format(
-            repr(self._index), repr(self.label))
+    def in_classes(self):
+        #TODO
+        pass
--- assignment/display.py
+++ assignment/display.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 from matplotlib.cm import gray
 from math import sqrt, floor
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def show_image(pixels, shape):
     """Display the provided pixels in the provided shape."""
     shaped_matrix = np.reshape(pixels, shape, order='F')
     image = plt.imshow(shaped_matrix, cmap=gray)
-    plt.show()
     return image
+
+
+def show_3d_classes(classes):
+    pass
